76c6fa9de05e34b4b3c45ba3ae1b2447dae7f1bf
Paras Patle <paras18224827@gmail.com>
2026-04-30 18:11:40 +0530

    cors policy changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,14 +12,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "capacitor://localhost",
-        "http://localhost",
-        "ionic://localhost",
-    ],
-    allow_credentials=True,
+    allow_origin_regex=".*",
     allow_methods=["*"],
     allow_headers=["*"],
 )
